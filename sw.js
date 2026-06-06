e079ae6a7332d6fb68b9784533eda31037a663d7
islamasloulou2009-droid <islamasloulou2009@gmail.com>
2026-06-06 16:07:03 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'niro-ai-v1';
+const urlsToCache = [
+  '/NIRO-QI/',
+  '/NIRO-QI/index.html'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
